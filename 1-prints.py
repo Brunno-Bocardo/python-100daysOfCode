a75528ded306fdedaaddf85cd72a32251b72b444
Brunno-Bocardo <brunno.pb@outlook.com>
2024-12-08 12:16:37 -0300

    day 1

---
+++ 1-prints.py
@@ -0,0 +1,24 @@
+# 1
+print("Hello world!")
+
+# 2
+print("\n1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
+
+# 3
+print("\nHello world!\nHello world!\nHello world!")
+
+# 4 
+print("\nHello " + "Brunno")
+
+# 5 
+name = input("\nWhat's your name? ")
+print(f"Welcome, {name}")
+print("Hello " + input("What is your name? "))
+
+# 6
+print(len(input("Qual o seu nome?")))
+

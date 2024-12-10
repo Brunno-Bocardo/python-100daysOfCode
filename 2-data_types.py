7a2a0d60f7af68a3090ed5e1977a90a057f6cc00
Brunno-Bocardo <brunno.pb@outlook.com>
2024-12-10 08:00:00 -0300

    day 2 first part

---
+++ 2-data_types.py
@@ -0,0 +1,31 @@
+# String
+print("This is a string")
+print("This is a string"[9]) # nona letra
+print("This is a string"[-9]) # nona letra de trás pra frente
+
+# Integer
+print(26)
+print(29 + 57)
+
+# Float
+print(3.14)
+
+# Large Integers
+print(123_456_789) #just make the number more readable
+
+# Boolean
+print(True)
+print(False)
+
+
+# ============================================================================
+
+# Podemos descobrir o typo de um dado ou variável
+type(123)
+fruta = "banana"
+print(type(fruta))
+
+
+# Type conversion / Type Casting
+print("123" + "76") # concatenação, não soma
+print(int("123") + int("76"))  # soma

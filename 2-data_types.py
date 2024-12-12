196d05a293398bc6bf33d6be3d21f731bd5dcbc4
Brunno-Bocardo <brunno.pb@outlook.com>
2024-12-12 08:14:07 -0300

    day 2

--- 2-data_types.py
+++ 2-data_types.py
@@ -29,3 +29,27 @@ print(type(fruta))
 # Type conversion / Type Casting
 print("123" + "76") # concatenação, não soma
 print(int("123") + int("76"))  # soma
+
+
+# ============================================================================
+
+print(6 / 2) # 3.0
+# numa divisão, sempre retornará um float
+
+print(6 // 2) # dessa forma, elimuna as casas decimais 
+
+
+# ============================================================================
+
+# Potência
+print(2 ** 3)
+
+
+# ============================================================================
+
+decimal_number = 8.85
+print(round(decimal_number)) # 9
+decimal_number = 3.3
+print(round(decimal_number)) # 3
+large_decimal_number = 6.7358409584640
+print(round(large_decimal_number, 2)) # 6.74

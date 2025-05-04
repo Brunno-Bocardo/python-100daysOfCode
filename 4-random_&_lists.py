04fa9d6be31e6cdb0014496f671e8076b3e79fd0
Brunno-Bocardo <brunno.pb@outlook.com>
2025-05-04 11:59:58 -0300

    Randomization

---
+++ 4-random_&_lists.py
@@ -0,0 +1,24 @@
+# Randomization and Lists
+
+# 1. Randomization
+# Randomization is the process of making something random. 
+# - In practice, randomization is not truly random, but rather pseudo-random. 
+# - This video explains that: https://www.khanacademy.org/computing/computer-science/cryptography/crypt/v/random-vs-pseudorandom-number-generators
+# In Python, we can use the `random` module to generate random numbers, select random elements from a list, and shuffle lists.
+# - random documentation: https://docs.python.org/3/library/random.html
+
+import random
+
+random_number = random.randint(1, 10) # Generates a random integer between 1 and 10 (inclusive)
+print(random_number)
+
+
+
+
+
+
+
+# 2. Lists
+
+
+
